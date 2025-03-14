ef099fe33b8770130fe62f6785a350303c00ff72
Sky Immortal <ojeremiah721@gmail.com>
2025-03-14 23:52:52 +0100

    nav update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ChatLobbyComponent } from './components/chat-lobby/chat-lobby.component';
 import { TopRankedRoomsComponent } from './components/top-ranked-rooms/top-ranked-rooms.component';
+import { AddFriendsComponent } from './components/add-friends/add-friends.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -14,4 +15,5 @@ export const routes: Routes = [
   { path: 'chat/:roomId', component: ChatComponent },
   { path: 'profile/:userId', component: ProfileComponent },
   { path: 'top-rooms', component: TopRankedRoomsComponent },
+  { path: 'add-friends', component: AddFriendsComponent },
 ];
